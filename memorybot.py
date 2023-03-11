5fb97a2c2695cb82a1e9de002e10f10ea0810f7e
Avra <96898985+avrabyt@users.noreply.github.com>
2023-03-11 20:53:21 +0100

    Update memorybot.py

--- memorybot.py
+++ memorybot.py
@@ -103,9 +103,10 @@ st.subheader(" Powered by 🦜 LangChain + OpenAI + Streamlit")
 # Ask the user to enter their OpenAI API key
 API_O = st.sidebar.text_input("API-KEY", type="password")
 
-# Session state storage would be ideal
+# Not the ideal way - but will work for time being
 if not API_O:
-    st.stop()
+    st.sidebar.warning('API key required to try this app.The API key is not stored in any form.')
+
 # Load the ConversationChain object
 chain = load_conversation_chain(model=MODEL,k=K)     
 
